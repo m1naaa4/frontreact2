e24f5ebf8dc09f40cca17e5e55e216e81ce04318
Soulaimane <binedsoulaiman@gmail.com>
2021-04-18 00:18:27 +0000

    add 404 page for projects

--- src/Routes.js
+++ src/Routes.js
@@ -27,7 +27,9 @@ const Routes = (props) => {
                 
                         <Route exact path="/oops" component={Opps} />
 
-                        <Route component={NotFound} />
+                        <Route path="*" render={ props=>(
+                            <NotFound header="show" />
+                        )} />
                 </Switch>
             </LanguageProvider>
 
