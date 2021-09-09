acada80c9d5fab218b9aae4d003bde8de9597a8b
abdelkrim <ichiabdelkrim@gmail.com>
2021-09-09 19:36:16 +0100

    start work on select with search

--- react-app/src/views/User/Fields/Filter/Project/ZoneDropFilter.js
+++ react-app/src/views/User/Fields/Filter/Project/ZoneDropFilter.js
@@ -1,17 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import { useTranslation } from 'react-i18next';
 import countries from '../../../../../data/countries'
+import Select from 'react-select'
 
 
 function ZoneDropFilter ({field, label, ...others }) {
     const { t, i18n } = useTranslation();
+    const [ariaFocusMessage, setAriaFocusMessage] = useState('');
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+    const style = {
+        blockquote: {
+          fontStyle: 'italic',
+          fontSize: '.75rem',
+          margin: '1rem 0',
+        },
+        label: {
+          fontSize: '.75rem',
+          fontWeight: 'bold',
+          lineHeight: 2,
+        },
+      };
+
+      const onFocus = ({ focused, isDisabled }) => {
+        const msg = `You are currently focused on option ${focused.label}${
+          isDisabled ? ', disabled' : ''
+        }`;
+        setAriaFocusMessage(msg);
+        return msg;
+      };
+
+      const onMenuOpen = () => setIsMenuOpen(true);
+      const onMenuClose = () => setIsMenuOpen(false);
 
     return (
-        <select className="user-type" name={field} {...others} required={others.required && "required"}>
-            {countries.map((item) => (
-                <option key={item.value} value={item.value} >{item.label}</option>
-            ))}
-        </select>
+        // <select className="user-type" name={field} {...others} required={others.required && "required"}>
+        //     {countries.map((item) => (
+        //         <option key={item.value} value={item.value} >{item.label}</option>
+        //     ))}
+        // </select>
+        // <Select className="user-type" options={countries} name={field} {...others} required={others.required && "required"}/>
+        <Select 
+        className="user-type"
+        name={field}
+        inputId="aria-example-input"
+        onMenuOpen={onMenuOpen}
+        onMenuClose={onMenuClose}
+        options={countries}
+      />
     )
 }
 
