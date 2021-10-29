bbb11f79a0c0b43101c851eaa070706655bcff4f
abdelkrim dell <ichiabdelkrim@gmail.com>
2021-10-29 01:32:24 +0100

    add servcer api url to env

--- react-app/src/Config/Production.js
+++ react-app/src/Config/Production.js
@@ -2,7 +2,7 @@ const prod = {
     urls: {
         'front' : 'https://test.dadupa.com',
        // 'api' : 'https://gatwaytest.dadupa.com/api'
-        'api'   : `${process.env.REACT_APP_API_PROD}`
+        'api'   : `${process.env.REACT_APP_API_URL}`
     },
     facebook:{
         appId      : '2711133325873185'
