863ecf392c8f27e469a44afc5940cae75cd76e6b
anasbouabid <bouabidanas02@gmail.com>
2022-07-20 23:22:30 +0100

    added environement variable instead of using a hard coded value

--- react-app/src/services/Pusher.js
+++ react-app/src/services/Pusher.js
@@ -9,7 +9,7 @@ class PusherService {
         key: 'local',
         cluster: 'eu',//`${process.env.CLUSTER}`,
         encrypted: true,
-        wsHost: 'api.dadupa.com',
+        wsHost: `${process.env.WS_HOST}`,
         wsPort: 6001,
         wssPort: 6001,
         forceTLS: true,
