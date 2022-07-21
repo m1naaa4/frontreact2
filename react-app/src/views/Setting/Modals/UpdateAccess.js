c933343d3d3065678e8f467bc7af010beadfb008
anasbouabid <bouabidanas02@gmail.com>
2022-07-21 18:29:18 +0100

    applied the modal design to the settings page

--- react-app/src/views/Setting/Modals/UpdateAccess.js
+++ react-app/src/views/Setting/Modals/UpdateAccess.js
@@ -5,7 +5,7 @@ import { AccessUserAction } from '../../../store/actions/Profile/UserActions';
 
 
 
-const  UpdateAccess = ({ show, handleClose}) => {
+const UpdateAccess = ({ show, handleClose }) => {
     const dispatch = useDispatch();
 
     const [passwordShown, setPasswordShown] = useState(false);
@@ -18,59 +18,56 @@ const  UpdateAccess = ({ show, handleClose}) => {
         setPasswordShownnew(passwordShownnew ? false : true);
     };
 
-    const [formData, setForm] = useForm({email:'', password:'', newpassword:''});
+    const [formData, setForm] = useForm({ email: '', password: '', newpassword: '' });
 
-    const updateAccess =(id) =>{
+    const updateAccess = (id) => {
         console.log(formData)
         dispatch(AccessUserAction(formData, '', ''));
-      }
-    
-    return (  
-      <>
-      {
-     show &&
-     
-        <div class="ModalUpdate" role="document">
-        <div class="ModalUpdate-Content modal-content">
-            <div className="User-Settings">
-             <div className="form-inputs">
-                
-                <div className="form-row">
+    }
 
-                {/* <h4>Updating Email</h4>
-                <div className="col-md-12 input-row">
-                    <input type="text" name="project-name" value="" placeholder="Actuel email" className="wizard-required" required />
-                </div>
-                <div className="col-md-12 input-row">
-                    <input type="text" name="project-name" value="" placeholder="Nouveau email" className="wizard-required" required />
-                </div> */}
-                {/* <div className="col-md-12 input-row">
-                    <input type="text" name="project-name" value="" placeholder="Confirmer nouvel email" className="wizard-required" required />
-                </div> */}
+    return (
+        <>
+            {
+                show &&
+                <div className="modal-body">
+                    <div className="form-inputs">
+                        <div className="User-Settings-Header">
+                            <h3 className="Profile-Section-Title"><i className="uil uil-keyhole-circle"></i> Updating Password</h3>
+                        </div>
+                        <div className="form-row">
 
-                {/* <h4>Updating Password</h4> */}
-                <div className="col-md-12 input-row">
-                    <input name="password" defaultValue="" type={passwordShown ? "text" : "password"}
-                            onChange={setForm} placeholder="Actuel mot de passe" className="wizard-required" required />
-                            <span toggle="#password-field" onClick={TogglePasswordVisiblity}
-                                                      className="uil uil-eye field-icon toggle-password"></span>
+                            {/* <h4>Updating Email</h4>
+                    <div className="col-md-12 input-row">
+                        <input type="text" name="project-name" value="" placeholder="Actuel email" className="wizard-required" required />
+                    </div>
+                    <div className="col-md-12 input-row">
+                        <input type="text" name="project-name" value="" placeholder="Nouveau email" className="wizard-required" required />
+                    </div> */}
+                            {/* <div className="col-md-12 input-row">
+                        <input type="text" name="project-name" value="" placeholder="Confirmer nouvel email" className="wizard-required" required />
+                    </div> */}
+
+                            {/* <h4>Updating Password</h4> */}
+                            <div className="col-md-12 input-row">
+                                <input name="password" defaultValue="" type={passwordShown ? "text" : "password"}
+                                    onChange={setForm} placeholder="Actuel mot de passe" className="wizard-required" required />
+                                <span toggle="#password-field" onClick={TogglePasswordVisiblity}
+                                    className="uil uil-eye field-icon toggle-password"></span>
+                            </div>
+                            <div className="col-md-12 input-row">
+                                <input name="newpassword" defaultValue="" type={passwordShownnew ? "text" : "password"}
+                                    onChange={setForm} placeholder="Nouveau mot de passe" className="wizard-required" required />
+                                <span toggle="#password-field" onClick={TogglePasswordVisiblitynew}
+                                    className="uil uil-eye field-icon toggle-password"></span>
+                            </div>
+                            <div className="DadupaModal-Footer w-100">
+                                <button type="submit" className="DadupaModal-BTNSubmit" name="submit" onClick={() => { updateAccess(); handleClose() }}>Update</button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div className="col-md-12 input-row">
-                    <input name="newpassword" defaultValue="" type={passwordShownnew ? "text" : "password"}
-                            onChange={setForm} placeholder="Nouveau mot de passe" className="wizard-required" required />
-                            <span toggle="#password-field" onClick={TogglePasswordVisiblitynew}
-                                                      className="uil uil-eye field-icon toggle-password"></span>
-                </div>
-                <div className="User-Settings-Footer">
-                    <button type="submit" className="DadupaModal-BTNSubmit" name="submit" onClick={() => {updateAccess(); handleClose()}}>Update</button>
-                </div>
-                </div>
-            </div>
-            </div>
-        </div>
-        </div>
-      }
-      </>
+            }
+        </>
     )
 }
 export default UpdateAccess;
