921b06fc4f60ef30057a1cf54b5e810df402f72f
anasbouabid <bouabidanas02@gmail.com>
2022-08-03 19:52:33 +0100

    small design change in offer

--- react-app/src/views/Projects/ProjectGridView.js
+++ react-app/src/views/Projects/ProjectGridView.js
@@ -1,10 +1,10 @@
-import React, { useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link, NavLink } from 'react-router-dom';
-import {Text} from "../../containers/Language";
+import { Text } from "../../containers/Language";
 import { Player } from 'video-react';
 import VideoPlayer from 'simple-react-video-thumbnail'
 import SharePopUp from '../../utils/SharePopUp'
-import {countryName} from '../../helpers/Helpres'
+import { countryName } from '../../helpers/Helpres'
 import config from '../../Config'
 import slugify from 'react-slugify';
 import { useHistory } from "react-router-dom";
@@ -16,15 +16,15 @@ const ProjectGridView = ({ project }) => {
     const [classe, setClasse] = useState(project?.favorite);
     const dispatch = useDispatch();
     let history = useHistory();
-   
-    let url_to_share = slugify(project.name, { prefix: config.urls.front+'/project/show/'+project.id });
+
+    let url_to_share = slugify(project.name, { prefix: config.urls.front + '/project/show/' + project.id });
 
     const addTofavorite = (id) => {
         setClasse(!classe)
         let data = {
-            'url' : 'favorite/addToFavorite',
-            'provider_id' : id,
-            'provider' : 'project',
+            'url': 'favorite/addToFavorite',
+            'provider_id': id,
+            'provider': 'project',
         }
         dispatch(AddFavoriteAction(data))
     }
@@ -32,84 +32,83 @@ const ProjectGridView = ({ project }) => {
     const goToShowproject = (id) => {
         localStorage.setItem('provider', 'project')
         localStorage.setItem('provider_name', project.name)
-        history.push('/project/show/'+ id)
-     };
+        history.push('/project/show/' + id)
+    };
     return (
 
-             <div className="offer-box">
-                    <div className="offer-header" >
-                      {/* style={project.visibility ==='public'  ? {backgroundColor: '#F3FFF8'} : {}}> */}
-                        <div className="offer-title">
-                            {(project.logo_link==='/assets/images/porject-logo.png')?<img src={project.logo_link} style={{ height: "35px" , width: "45px"}}  title="Nom du projet" alt=""/>: 
-                            <img src={project.logo_link} style={{ height: "35px" , width: "45px",borderRadius: "10px"}}  title="Nom du projet" alt=""/>}
-                            
-                            <h3><span onClick={() => goToShowproject(project.id) } data-toggle="tooltip" data-placement="top" title={project.name}>
-                                {project.name.substring(0, 10)}</span></h3>
-                            <div className='footer-title'>
-                                <span className='mr-5'>{project.sector && (project.sector.charAt(0).toUpperCase() + project.sector.slice(1))}, </span>
-                                {project.owner && project.owner.map((value) => 
-                                    {
-                                        return <Link to={`/profile/${value.profile_id}`} data-toggle="tooltip" data-placement="top" title={value.username}>
-                                            {value.username.substring(0, 6)}
-                                        </Link>
-                                    }
-                                )} 
-                                <span>{project.visibility == 'public' ? <i className="uil uil-globe"></i> : ''}</span>
-                            </div>
-                        </div>
-                        <div className="offer-logo">
-                            <button className={`${classe ? 'near-deadline' : ''} offer-bookmark`} onClick={e => addTofavorite(project.id)} type="button" name="button" data-toggle="tooltip" data-placement="bottom" title="Enregistrer">
-                                    {console.log(classe)}
-                                <i className={classe ? 'uis uis-bookmark' : 'uil uil-bookmark'} ></i>
-                            </button>
-                        </div>
+        <div className="offer-box">
+            <div className="offer-header" >
+                {/* style={project.visibility ==='public'  ? {backgroundColor: '#F3FFF8'} : {}}> */}
+                <div className="offer-title">
+                    {(project.logo_link === '/assets/images/porject-logo.png') ? <img src={project.logo_link} style={{width: "45px" }} title="Nom du projet" alt="" /> :
+                        <img src={project.logo_link} style={{ width: "45px", }} title="Nom du projet" alt="" />}
+
+                    <h3><span onClick={() => goToShowproject(project.id)} data-toggle="tooltip" data-placement="top" title={project.name}>
+                        {project.name.substring(0, 10)}</span></h3>
+                    <div className='footer-title'>
+                        <span className='mr-5'>{project.sector && (project.sector.charAt(0).toUpperCase() + project.sector.slice(1))}, </span>
+                        {project.owner && project.owner.map((value) => {
+                            return <Link to={`/profile/${value.profile_id}`} data-toggle="tooltip" data-placement="top" title={value.username}>
+                                {value.username.substring(0, 6)}
+                            </Link>
+                        }
+                        )}
+                        <span>{project.visibility == 'public' ? <i className="uil uil-globe"></i> : ''}</span>
                     </div>
-                    <div className="offer-media">
-                        {
-                            project.is_video ? (
-                                <img width="100%" height="300" src={project.media_link} alt="Project"/>
-                                ) : (<img width="100%" height="300" src={project.media_link} alt="Project"/>)
-                        } 
-                    </div>
-                    <div className="offer-meta">
-                        <ul className="meta-items">
-                            <li className="meta-item">
-                                <div className="meta-icon">
-                                    <img src="/assets/images/icons/marker.svg" alt=""/>
-                                </div>
-                                <div className="meta-details">
-                                    <span className="meta-title" onClick={() => goToShowproject(project.id) }><Text tid="targetAreas" /></span>
-                                    <span className="meta-value">{countryName(project.project_area)}</span>
-                                </div>
-                            </li>
-                            <li className="meta-item">
-                                <div className="meta-icon">
-                                    <img src="/assets/images/icons/cost.svg" alt=""/>
-                                </div>
-                                <div className="meta-details">
-                                    <span className="meta-title"><Text tid="funding" /></span>
-                                    <span className="meta-value">{project.funding_search}</span>
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
-                    <div className="offer-reactions">
-                        <ul className="reactions-box">
-                            <li className="reaction likes"><i className="dadupa-icon icon-clap"></i>
-                                <span>{project.likeCount}</span></li>
-                            <li className="reaction views"><i className="uil uil-eye"></i>
-                                <span>1500</span></li>
-                            <li className="reaction comments"><i
-                                className="uil uil-comment-dots"></i> <span>{project.commentCount}</span>
-                            </li>
-                            <li className="reaction shares" onClick={() => setShareUrl(true)}>
-                                <i className="uil uil-share-alt"></i>
-                                <span>380 Shares</span>
-                            </li>
-                        </ul>
-                    </div>
-                    <SharePopUp url={url_to_share} open={shareUrl} handleOpen={setShareUrl}></SharePopUp>
                 </div>
+                <div className="offer-logo">
+                    <button className={`${classe ? 'near-deadline' : ''} offer-bookmark`} onClick={e => addTofavorite(project.id)} type="button" name="button" data-toggle="tooltip" data-placement="bottom" title="Enregistrer">
+                        {console.log(classe)}
+                        <i className={classe ? 'uis uis-bookmark' : 'uil uil-bookmark'} ></i>
+                    </button>
+                </div>
+            </div>
+            <div className="offer-media">
+                {
+                    project.is_video ? (
+                        <img width="100%" height="300" src={project.media_link} alt="Project" />
+                    ) : (<img width="100%" height="300" src={project.media_link} alt="Project" />)
+                }
+            </div>
+            <div className="offer-meta">
+                <ul className="meta-items">
+                    <li className="meta-item">
+                        <div className="meta-icon">
+                            <img src="/assets/images/icons/marker.svg" alt="" />
+                        </div>
+                        <div className="meta-details">
+                            <span className="meta-title" onClick={() => goToShowproject(project.id)}><Text tid="targetAreas" /></span>
+                            <span className="meta-value">{countryName(project.project_area)}</span>
+                        </div>
+                    </li>
+                    <li className="meta-item">
+                        <div className="meta-icon">
+                            <img src="/assets/images/icons/cost.svg" alt="" />
+                        </div>
+                        <div className="meta-details">
+                            <span className="meta-title"><Text tid="funding" /></span>
+                            <span className="meta-value">{project.funding_search}</span>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <div className="offer-reactions">
+                <ul className="reactions-box">
+                    <li className="reaction likes"><i className="dadupa-icon icon-clap"></i>
+                        <span>{project.likeCount}</span></li>
+                    <li className="reaction views"><i className="uil uil-eye"></i>
+                        <span>1500</span></li>
+                    <li className="reaction comments"><i
+                        className="uil uil-comment-dots"></i> <span>{project.commentCount}</span>
+                    </li>
+                    <li className="reaction shares" onClick={() => setShareUrl(true)}>
+                        <i className="uil uil-share-alt"></i>
+                        <span>380 Shares</span>
+                    </li>
+                </ul>
+            </div>
+            <SharePopUp url={url_to_share} open={shareUrl} handleOpen={setShareUrl}></SharePopUp>
+        </div>
 
     )
 }
