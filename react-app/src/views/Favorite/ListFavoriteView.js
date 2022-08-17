116ba0d9c76f87b8327ba8cbb3808489e7af513e
anasbouabid <bouabidanas02@gmail.com>
2022-08-17 23:51:09 +0100

    created the no content section as a component

--- react-app/src/views/Favorite/ListFavoriteView.js
+++ react-app/src/views/Favorite/ListFavoriteView.js
@@ -1,8 +1,8 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { ListFavoritesAction } from '../../store/actions/Favorite/FavoritesAction';
+import NoContent from '../../utils/NoContent';
 import FavoriteGrid from './FavoriteGrid';
-import { useHistory } from 'react-router';
 
 
 export default function ListFavoriteView(props) {
@@ -14,7 +14,6 @@ export default function ListFavoriteView(props) {
     }
     dispatch(ListFavoritesAction(data));
   }, [])
-  const history = useHistory();
 
   const favorites = useSelector(state => state.userProfile.favorites);
 
@@ -44,17 +43,7 @@ export default function ListFavoriteView(props) {
       </div>
     )
   } else {
-    return (
-      <div className='single-header mt-5'>
-        <h3 style={{ fontSize: "16px !important" }} className="single-offer-name">No content available</h3>
-
-        <img style={{ height: '50vh' }} src="/assets/images/no-data.png" alt="No content available" />
-
-        <button onClick={history.goBack} style={{ width: '200px' }} name="previous" className="previous action-button">
-          <i className="uil uil-arrow-left  "></i> Previous
-        </button>
-      </div>
-    )
+    return <NoContent/>
   }
 
 }
