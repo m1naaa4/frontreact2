01264323313ab2ef34c135718aa4344eb5fa64b2
Badr <jarmouni.bdr@gmail.com>
2022-08-22 17:26:19 +0100

    second commit on Reset Password section

--- react-app/src/views/User/Auth/ResetPasswordView.js
+++ react-app/src/views/User/Auth/ResetPasswordView.js
@@ -9,30 +9,39 @@ import { useTranslation } from "react-i18next";
 import ItemForm from "./ItemForm";
 import { Text } from "../../../containers/Language";
 import { useFormFields } from "../../../helpers/hooksFormInput";
+import { resetWarningCache } from "prop-types";
+import Resetpassword from "../../../pages/User/Auth/Resetpassword";
+import axios from "axios";
+import config from "../../../Config";
 
-function ResetPasswordView() {
+function ResetPasswordView(props) {
   const [t, i18n] = useTranslation();
-
+  const dispatch = useDispatch();
   const [fields, setfield] = useFormFields({
     email: "",
   });
+
+  const Resetpassword = () => {
+    console.log("reset password");
+  };
 
   return (
     <div>
       <HeaderLogo />
 
       <div className="Dadupa-Login">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-6 col-lg-8 d-none d-sm-block d-md-none d-lg-block">
-              <div className="page-image">
-                <img
-                  src="/assets/images/Reset-password.svg"
-                  alt="Dadupa Connect"
-                />
-              </div>
-            </div>
-            <div className="col-md-12 col-lg-4">
+        <div
+          className="container"
+          style={{
+            width: "40%",
+            height: "370px",
+            border: "10px solid #00CC66",
+            borderTopRightRadius: "20px",
+            borderBottomLeftRadius: "20px",
+          }}
+        >
+          <div>
+            <div>
               <div className="form-wrapper">
                 <form
                   id="form-login"
@@ -41,17 +50,29 @@ function ResetPasswordView() {
                   }}
                   className="form-login"
                 >
-                  <h3 className="form-title" style={{ fontSize: "30px" }}>
-                    Reset Password!
+                  <div className="login-link" style={{marginBottom:"20px"}}>
+                    <span style={{marginRight:"20px",fontSize:"15px"}}>{t("Vous n'avez pas de compte?")} </span>
+                    <NavLink to="/register" style={{textDecoration:"none"}}> 
+                       <span style={{ border:"3px solid #00CC66",paddingLeft:"15px",paddingRight:"15px",paddingTop:"5px",paddingBottom:"5px",fontSize: "12px",borderTopRightRadius: "10px",borderBottomLeftRadius:"10px" }}>{t("S'INSCRIRE")}</span> 
+                    </NavLink>
+                  </div>
+                  <h3
+                    className="form-title"
+                    style={{ fontSize: "20px", paddingBottom: "30px" }}
+                  >
+                    Réinitialiser le mot de passe
                   </h3>
-                  <div className="form-inputs">
+                  <div
+                    className="form-inputs"
+                    style={{ paddingLeft: "10px", paddingRight: "10px",marginBottom:"35px" }}
+                  >
                     <div className="input-row">
                       <ItemForm
                         type="email"
                         name="email"
                         value={fields.email}
                         onChange={setfield}
-                        placeholder="Type your email addresse"
+                        placeholder={t("Adresse électronique")}
                         margin="normal"
                         variant="outlined"
                         id="email"
@@ -60,9 +81,22 @@ function ResetPasswordView() {
                       />
                     </div>
                   </div>
-                  <div className="form-submit">
-                    <button type="submit" name="submit">
-                      {t(" Click to reset")}
+                  <div
+                    className="form-submit"
+                    style={{
+                      display: "flex",
+                      flexDirection: "row",
+                      justifyContent: "center",
+                      alignItems: "center",
+                    }}
+                  >
+                    <button
+                      type="submit"
+                      name="submit"
+                      onClick={Resetpassword}
+                      style={{ width: "50%", marginBottom: "50px",borderTopLeftRadius: "0px",borderBottomRightRadius:"0px",borderTopRightRadius: "15px",borderBottomLeftRadius:"15px" }}
+                    >
+                      {t("RÉINITIALISER")}
                     </button>
                   </div>
                 </form>
