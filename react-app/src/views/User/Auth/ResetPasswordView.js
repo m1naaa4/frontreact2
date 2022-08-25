e8e4ec8dd040f5e6d7565ac2318139b42b4546ba
Badr <jarmouni.bdr@gmail.com>
2022-08-25 18:04:18 +0100

    reset password first step done!

--- react-app/src/views/User/Auth/ResetPasswordView.js
+++ react-app/src/views/User/Auth/ResetPasswordView.js
@@ -10,39 +10,72 @@ import ItemForm from "./ItemForm";
 import { Text } from "../../../containers/Language";
 import { useFormFields } from "../../../helpers/hooksFormInput";
 import { resetWarningCache } from "prop-types";
-import Resetpassword from "../../../pages/User/Auth/Resetpassword";
 import axios from "axios";
 import config from "../../../Config";
+import { ResetpasswordAction } from '../../../store/actions/User/Auth/AuthActions'
 
 function ResetPasswordView(props) {
   const [t, i18n] = useTranslation();
   const dispatch = useDispatch();
+  const authResponse = useSelector(state => state.userAuth.authResponse);
   const [fields, setfield] = useFormFields({
     email: "",
   });
 
-  const Resetpassword = () => {
-    console.log("reset password");
-  };
+  // const Resetpassword = async () => {
+  //   const res = await axios.post("/password/create", fields);
+  //   if (res.status === 200) {
+  //     setSuccess(true);
+  //     console.log(res.data);
+  //   } else {
+  //     setSuccess(false);
+  //     console.log(res.data);
+  //   }
+  // };
+
+  const HandleReset = ()=>{
+
+    dispatch(ResetpasswordAction(fields, props.props));
+    console.log(authResponse)
+  }
 
   return (
     <div>
       <HeaderLogo />
 
       <div className="Dadupa-Login">
-        <div
-          className="container"
-          style={{
-            width: "40%",
-            height: "370px",
-            border: "10px solid #00CC66",
-            borderTopRightRadius: "20px",
-            borderBottomLeftRadius: "20px",
-          }}
-        >
-          <div>
-            <div>
-              <div className="form-wrapper">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6 col-lg-7 d-none d-sm-block d-md-none d-lg-block">
+              <div className="page-image">
+                <img
+                  src="/assets/images/Forgotpassword_Illustration.svg"
+                  alt="Dadupa Connect"
+                />
+              </div>
+            </div>
+            <div className="col-md-6 col-lg-5">
+              <div
+                className="form-wrapper"
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  border: "10px solid #00CC66",
+                  borderTopRightRadius: "20px",
+                  borderBottomLeftRadius: "20px",
+                  padding: "10px",
+                }}
+              >
+                {authResponse === 'false' && (
+                  <div className="alert alert-danger">
+                     Please verify if the email is correct and try again!
+                  </div>
+                )}
+                {authResponse === 'true'  && (
+                  <div className="alert alert-success">
+                     Please Check Your Inbox to reset your Password!
+                  </div>
+                )}
                 <form
                   id="form-login"
                   onSubmit={(e) => {
@@ -50,10 +83,25 @@ function ResetPasswordView(props) {
                   }}
                   className="form-login"
                 >
-                  <div className="login-link" style={{marginBottom:"20px"}}>
-                    <span style={{marginRight:"20px",fontSize:"15px"}}>{t("Vous n'avez pas de compte?")} </span>
-                    <NavLink to="/register" style={{textDecoration:"none"}}> 
-                       <span style={{ border:"3px solid #00CC66",paddingLeft:"15px",paddingRight:"15px",paddingTop:"5px",paddingBottom:"5px",fontSize: "12px",borderTopRightRadius: "10px",borderBottomLeftRadius:"10px" }}>{t("S'INSCRIRE")}</span> 
+                  <div className="login-link" style={{ marginBottom: "20px" }}>
+                    <span style={{ marginRight: "20px", fontSize: "15px" }}>
+                      {t("Vous n'avez pas de compte?")}{" "}
+                    </span>
+                    <NavLink to="/register" style={{ textDecoration: "none" }}>
+                      <span
+                        style={{
+                          border: "3px solid #00CC66",
+                          paddingLeft: "15px",
+                          paddingRight: "15px",
+                          paddingTop: "5px",
+                          paddingBottom: "5px",
+                          fontSize: "12px",
+                          borderTopRightRadius: "10px",
+                          borderBottomLeftRadius: "10px",
+                        }}
+                      >
+                        {t("S'INSCRIRE")}
+                      </span>
                     </NavLink>
                   </div>
                   <h3
@@ -64,7 +112,11 @@ function ResetPasswordView(props) {
                   </h3>
                   <div
                     className="form-inputs"
-                    style={{ paddingLeft: "10px", paddingRight: "10px",marginBottom:"35px" }}
+                    style={{
+                      paddingLeft: "10px",
+                      paddingRight: "10px",
+                      marginBottom: "35px",
+                    }}
                   >
                     <div className="input-row">
                       <ItemForm
@@ -93,8 +145,15 @@ function ResetPasswordView(props) {
                     <button
                       type="submit"
                       name="submit"
-                      onClick={Resetpassword}
-                      style={{ width: "50%", marginBottom: "50px",borderTopLeftRadius: "0px",borderBottomRightRadius:"0px",borderTopRightRadius: "15px",borderBottomLeftRadius:"15px" }}
+                      onClick={HandleReset}
+                      style={{
+                        width: "50%",
+                        marginBottom: "50px",
+                        borderTopLeftRadius: "0px",
+                        borderBottomRightRadius: "0px",
+                        borderTopRightRadius: "15px",
+                        borderBottomLeftRadius: "15px",
+                      }}
                     >
                       {t("RÉINITIALISER")}
                     </button>
